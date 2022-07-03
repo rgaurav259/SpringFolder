b8960a2f312df093eaed58357f416034fcef276f
Gaurav Kumar <rgaurav259@gmail.com>
2022-07-03 15:40:39 +0530

    Added functionalities

--- JpaExample/src/main/java/com/example/jpaDemo/Jpa/Entity/Employee.java
+++ JpaExample/src/main/java/com/example/jpaDemo/Jpa/Entity/Employee.java
@@ -1,22 +1,29 @@
 package com.example.jpaDemo.Jpa.Entity;
 
-import lombok.Generated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long empid;
+    private Long empId;
     private String name;
     private Integer age;
     @Column(name = "PHONE_NUMBER")
-    private Long Longphonenumber;
+    private Long phonenumber =(long) (Math.random()*Math.pow(10,10));
     private String designation;
-    private Double salary;
+    private Double salary =Math.random()*100000;
 
-
+//if you dont't want to every time typed phone number you cnad do one thing
+    // generated auto value using math.random fundctiosn
 
 }
--- JpaExample/src/main/java/com/example/jpaDemo/Jpa/Repository/EmployeeRepositroy.java
+++ JpaExample/src/main/java/com/example/jpaDemo/Jpa/Repository/EmployeeRepositroy.java
@@ -3,5 +3,19 @@ package com.example.jpaDemo.Jpa.Repository;
 import com.example.jpaDemo.Jpa.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepositroy extends JpaRepository<Employee,Long> {
+
+    Employee findByEmpId(Long id);
+
+    List<Employee> deleteByDesignation(String designation);
+
+
+    Employee findTopByDesignationOrderBySalaryDesc(String designation);
+
+
+    List<Employee> findByName(String name);
+
+
 }
