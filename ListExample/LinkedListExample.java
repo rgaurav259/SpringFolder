0d2d749b534c8cb837c74121495eea5517efe70d
Gaurav Kumar <rgaurav259@gmail.com>
2022-07-18 12:11:50 +0530

    new folder added collection framework

---
+++ ListExample/LinkedListExample.java
@@ -0,0 +1,24 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList l=new LinkedList<>();
+        l.add("gaurav");
+        l.add(null);
+        l.add("gaurav");
+        System.out.println("l = " + l);
+        l.set(0,"software");
+        System.out.println("l = " + l);
+        l.set(0,"venky");
+        System.out.println("l = " + l);
+        l.removeLast();
+        System.out.println("l = " + l);
+        l.removeFirst();
+        System.out.println("l = " + l);
+        l.addFirst("CCCCCCCCC");
+        System.out.println("l = " + l);
+
+        
+    }
+}
